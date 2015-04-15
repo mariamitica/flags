b4bf2d8d3aa3414ecaa77b7ff1bb3791b2c18446
maria work <maria.mitica@gmail.com>
2015-04-15 13:12:37 +0300

    africa

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,8 +2,21 @@ var Flags = (function(){
 
 	function init() {
 		$('#africa').on('click', function(){
-				$('#continents').hide();
+				$('#continents').fadeOut(300);
+				$('#africa-big').fadeIn(3000);
 			});
+		$('#country-data').on('show.bs.modal', function (event) {
+		  var button = $(event.relatedTarget); // Button that triggered the modal
+		  var country = button.data('country'); // Extract info from data-* attributes
+		  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+		  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+		  console.log(country);
+		  console.log(button);
+		  var modal = $(this);
+		  modal.find('.modal-title').text(country);
+		  modal.find('.modal-body img').attr('src', '../static/images/flags/' + country + ".jpg");
+		});
+		
 	}
 	return {
 		init: init
